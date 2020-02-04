a315c98aaeda5e5c6f622a6a0c3822acdb38cb54
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-02-04 14:48:50 -0800

    added models and beginning of routes

---
+++ models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
+
+
+class UserModel(DB.Model):
+    __tablename__ = 'users'
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    username = DB.Column(DB.String(20), unique=True)
+    Users_Salty_Score = DB.Column(DB.BigInteger, nullable=False)
+
+    def __repr__(self):
+        return '<UserModel {}>'.format(self.username)
+
+
+class CommentModel(DB.Model):
+    __tablename__ = 'comments'
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(500), nullable=False)
+    username = DB.Column(DB.BigInteger, ForeignKey('users.username'))
+
+    def __repr__(self):
+        return '<CommentModel {}>'.format(self.username)
